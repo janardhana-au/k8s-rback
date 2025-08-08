8e0abe10f377992915daaca6c5d9325799245420
janardhana-au <janardhana.au@gmail.com>
2025-08-08 10:07:52 +0930

    added

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,7 @@ fi
 echo "Setting MYSQL_ROOT_PASSWORD  available in env"
 export MYSQL_ROOT_PASSWORD=$PASSWORD
 echo "Now MYSQL_ROOT_PASSWORD  available in env"
+echo "MYSQL_ROOT_PASSWORD is: '$MYSQL_ROOT_PASSWORD'"
+env | grep MYSQL_ROOT_PASSWORD
 rm -rf /tmp/mysql_root_password.txt
 exec /entrypoint.sh mysqld
